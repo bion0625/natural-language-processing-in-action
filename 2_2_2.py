2e6a938470989e1f7001e57aab74d05ce5e1ffbf
bion0625 <stxtory@gmail.com>
2025-06-30 09:53:20 +0900

    단어 분리를 위한 정규 표현식 개선

--- 2_2_2.py
+++ 2_2_2.py
@@ -20,3 +20,27 @@ print(f"result: {result}")
 
 result = df.sent0.dot(df.sent3)
 print(f"result: {result}")
+
+import re
+sentence = """Thomas Jefferson began building Monticello at the age of 26."""
+
+pattern = re.compile(r"([-\s,.!?])+")
+tokens = pattern.split(sentence)
+print(f"tokens[-10:]: {tokens[-10:]}")
+
+result = [x for x in tokens if x and x not in '- \t\n.,!?']
+print(f"result: {result}")
+
+result = list(filter(lambda x: x if x not in '- \t\n.,!?' else None, tokens))
+print(f"result: {result}")
+
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+|$[0-9.]+|\S+')
+result = tokenizer.tokenize(sentence)
+print(f"result: {result}")
+
+from nltk.tokenize import TreebankWordTokenizer
+sentence = """Monticello wasn't designated as UNESCO world Heritage Site until 1987."""
+tokenizer = TreebankWordTokenizer()
+result = tokenizer.tokenize(sentence)
+print(f"result: {result}")
